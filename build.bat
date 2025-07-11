e39c46dba6c8e4314933d0899d7196ad6eb5e73f
Baymaxawa <2502474438@qq.com>
2025-07-11 16:14:57 +0800

    updates

--- build.bat
+++ build.bat
@@ -1,13 +1,24 @@
 @echo off
-echo start build exe file
+echo go into venv
 echo Continue?
 pause > nul
-pyinstaller -y --clean --onefile --windowed -i pack/icon.ico main.py
+call .venv\Scripts\activate.bat
 if "%errorlevel%" neq "0" (
     echo Error when running this section!
     pause
     exit /b %errorlevel%
 )
+cls
+echo start build exe file
+echo Continue?
+pause > nul
+pyinstaller -y --onefile --windowed -i pack/icon.ico main.py
+if "%errorlevel%" neq "0" (
+    echo Error when running this section!
+    pause
+    exit /b %errorlevel%
+)
+cls
 echo change work directory
 echo Continue?
 pause > nul
@@ -17,109 +28,124 @@ if "%errorlevel%" neq "0" (
     pause
     exit /b %errorlevel%
 )
+cls
 echo copy necessary files
 echo Continue?
 pause > nul
-copy /Y BBPC.cfg BepInEx64/BepInEx/config/BBPC.cfg
-copy /Y BBPC.cfg BepInEx86/BepInEx/config/BBPC.cfg
-copy /Y header.png ../dist/pack/header.png
-copy /Y left.png ../dist/pack/left.png
-copy /Y items.json ../dist/pack/items.json
-copy /Y icon.ico ../dist/pack/icon.ico
-copy /Y LICENSE ../dist/pack/LICENSE
-copy /Y readme.txt ../dist/pack/readme.txt
+copy BBPC.cfg BepInEx64\BepInEx\config\BBPC.cfg /Y
+copy BBPC.cfg BepInEx86\BepInEx\config\BBPC.cfg /Y
+copy header.png ..\dist\pack\header.png /Y
+copy left.png ..\dist\pack\left.png /Y
+copy items.json ..\dist\pack\items.json /Y
+copy icon.ico ..\dist\pack\icon.ico /Y
+copy LICENSE ..\dist\pack\LICENSE /Y
+copy readme.txt ..\dist\pack\readme.txt /Y
 if "%errorlevel%" neq "0" (
     echo Error when running this section!
     pause
     exit /b %errorlevel%
 )
+cls
 echo pack main files
 echo Continue?
 pause > nul
-7z a BBDevAPI/. BBDevAPI.7z
-7z a BBPC/. BBPC.7z
-7z a BepInEx64/. BepInEx64.7z
-7z a BepInEx86/. BepInEx86.7z
+7z a BBDevAPI.7z BBDevAPI/.
+7z a BBPC.7z BBPC/.
+7z a BepInEx64.7z BepInEx64/.
+7z a BepInEx86.7z BepInEx86/.
 if "%errorlevel%" neq "0" (
     echo Error when running this section!
     pause
     exit /b %errorlevel%
 )
+cls
 echo copy main files
 echo Continue?
 pause > nul
-copy /Y BBDevAPI.7z ../dist/pack/BBDevAPI.7z
-copy /Y BBPC.7z ../dist/pack/BBPC.7z
-copy /Y BepInEx64.7z ../dist/pack/BepInEx64.7z
-copy /Y BepInEx86.7z ../dist/pack/BepInEx86.7z
+copy BBDevAPI.7z ..\dist\pack\BBDevAPI.7z /Y
+copy BBPC.7z ..\dist\pack\BBPC.7z /Y
+copy BepInEx64.7z ..\dist\pack\BepInEx64.7z /Y
+copy BepInEx86.7z ..\dist\pack\BepInEx86.7z /Y
 if "%errorlevel%" neq "0" (
     echo Error when running this section!
     pause
     exit /b %errorlevel%
 )
+cls
 echo pack minimal installer
 echo Continue?
 pause > nul
-7z a ../dist/. ../dist/BB+ 0.11 v1.12 Beta [MINI].7z
+7z a "../BB+ 0.11 v1.12 Beta [MINI].7z" ../dist/.
 if "%errorlevel%" neq "0" (
     echo Error when running this section!
     pause
     exit /b %errorlevel%
 )
+cls
 echo pack other files
 echo Continue?
 pause > nul
-7z a NullTeacher/. NullTeacher.7z
-7z a TeacherAPI/. TeacherAPI.7z
-7z a CustomMainMenuAPI/. CustomMainMenuAPI.7z
-7z a BBA-G/. BBA-G.7z
-7z a FunSettings/. FunSettings.7z
-7z a TweaksPlus/. TweaksPlus.7z
-7z a PixelInternalAPI/. PixelInternalAPI.7z
-7z a ExtraLockers/. ExtraLockers.7z
-7z a CustomMusics/. CustomMusics.7z
-7z a Animations/. Animations.7z
-7z a ModdedModesAPI/. ModdedModesAPI.7z
-7z a ChaoticChallenges/. ChaoticChallenges.7z
-7z a StackableItems/. StackableItems.7z
-7z a SeedExtension/. SeedExtension.7z
-7z a PineDebug/. PineDebug.7z
+7z a NullTeacher.7z NullTeacher/.
+7z a TeacherAPI.7z TeacherAPI/.
+7z a CustomMainMenuAPI.7z CustomMainMenuAPI/.
+7z a BBA-G.7z BBA-G/.
+7z a FunSettings.7z FunSettings/.
+7z a TweaksPlus.7z TweaksPlus/.
+7z a PixelInternalAPI.7z PixelInternalAPI/.
+7z a ExtraLockers.7z ExtraLockers/.
+7z a CustomMusics.7z CustomMusics/.
+7z a Animations.7z Animations/.
+7z a ModdedModesAPI.7z ModdedModesAPI/.
+7z a ChaoticChallenges.7z ChaoticChallenges/.
+7z a StackableItems.7z StackableItems/.
+7z a SeedExtension.7z SeedExtension/.
+7z a PineDebug.7z PineDebug/.
 if "%errorlevel%" neq "0" (
     echo Error when running this section!
     pause
     exit /b %errorlevel%
 )
+cls
 echo copy other files
 echo Continue?
 pause > nul
-copy /Y NullTeacher.7z ../dist/pack/NullTeacher.7z
-copy /Y TeacherAPI.7z ../dist/pack/TeacherAPI.7z
-copy /Y CustomMainMenuAPI.7z ../dist/pack/CustomMainMenuAPI.7z
-copy /Y BBA-G.7z ../dist/pack/BBA-G.7z
-copy /Y FunSettings.7z ../dist/pack/FunSettings.7z
-copy /Y TweaksPlus.7z ../dist/pack/TweaksPlus.7z
-copy /Y PixelInternalAPI.7z ../dist/pack/PixelInternalAPI.7z
-copy /Y ExtraLockers.7z ../dist/pack/ExtraLockers.7z
-copy /Y CustomMusics.7z ../dist/pack/CustomMusics.7z
-copy /Y Animations.7z ../dist/pack/Animations.7z
-copy /Y ModdedModesAPI.7z ../dist/pack/ModdedModesAPI.7z
-copy /Y ChaoticChallenges.7z ../dist/pack/ChaoticChallenges.7z
-copy /Y StackableItems.7z ../dist/pack/StackableItems.7z
-copy /Y SeedExtension.7z ../dist/pack/SeedExtension.7z
-copy /Y PineDebug.7z ../dist/pack/PineDebug.7z
+copy NullTeacher.7z ..\dist\pack\NullTeacher.7z /Y
+copy TeacherAPI.7z ..\dist\pack\TeacherAPI.7z /Y
+copy CustomMainMenuAPI.7z ..\dist\pack\CustomMainMenuAPI.7z /Y
+copy BBA-G.7z ..\dist\pack\BBA-G.7z /Y
+copy FunSettings.7z ..\dist\pack\FunSettings.7z /Y
+copy TweaksPlus.7z ..\dist\pack\TweaksPlus.7z /Y
+copy PixelInternalAPI.7z ..\dist\pack\PixelInternalAPI.7z /Y
+copy ExtraLockers.7z ..\dist\pack\ExtraLockers.7z /Y
+copy CustomMusics.7z ..\dist\pack\CustomMusics.7z /Y
+copy Animations.7z ..\dist\pack\Animations.7z /Y
+copy ModdedModesAPI.7z ..\dist\pack\ModdedModesAPI.7z /Y
+copy ChaoticChallenges.7z ..\dist\pack\ChaoticChallenges.7z /Y
+copy StackableItems.7z ..\dist\pack\StackableItems.7z /Y
+copy SeedExtension.7z ..\dist\pack\SeedExtension.7z /Y
+copy PineDebug.7z ..\dist\pack\PineDebug.7z /Y
 if "%errorlevel%" neq "0" (
     echo Error when running this section!
     pause
     exit /b %errorlevel%
 )
+cls
 echo pack full installer
 echo Continue?
 pause > nul
-7z a ../dist/. ../dist/BB+ 0.11 v1.12 Beta [FULL].7z
+7z a "../BB+ 0.11 v1.12 Beta [FULL].7z" ../dist/.
 if "%errorlevel%" neq "0" (
     echo Error when running this section!
     pause
     exit /b %errorlevel%
 )
+cls
+echo remove temp files
+echo Continue?
+pause > nul
+rd /S /Q ..\dist\pack
+mkdir ..\dist\pack
+del /S /Q ..\dist\metadata.json
+cls
 echo Done!
 pause
